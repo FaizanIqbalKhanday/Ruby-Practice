67d87bd79c87a52319755891953a6d9a085d827a
Faizan Iqbal <faizaniqbal263@gmail.com>
2025-05-27 19:40:07 +0530

    complete calculator

--- prtc1.rb
+++ prtc1.rb
@@ -1,4 +1,19 @@
 # basic aerthimatic operations
+def multiplication(num1 ,num2)
+    num1.to_f*num2.to_f
+end
+def addition(num1 ,num2)
+    num1.to_f+num2.to_f
+end
+def subtraction(num1 ,num2)
+    num1.to_f-num2.to_f
+end
+def division(num1 ,num2)
+    num1.to_f/num2.to_f
+end
+def modulus(num1 ,num2)
+    num1.to_f%num2.to_f
+end
 puts "basic calculator"
 20.times{print "_"}
 puts
@@ -6,13 +21,22 @@ puts "num1 is"
 num1 =gets.chomp
 puts "num2 is"
 num2 =gets.chomp
-multiplication =num1.to_i*num2.to_i
-sum=num1.to_i+num2.to_i
-subtract=num1.to_i-num2.to_i
-division =num1.to_i/num2.to_i
-modulus =num1.to_i%num2.to_i
-puts "multiplication of #{num1} and #{num2} is #{multiplication} "
-puts "addition of #{num1} and #{num2} is #{sum} "
-puts "subtraction of #{num1} and #{num2} is #{subtract} "
-puts "division of #{num1} and #{num2} is #{division} "
-puts "modulus of #{num1} and #{num2} is #{modulus} "
+puts "enter any operator like +,-,*,\/,and %"
+operator =gets.chomp
+if operator=="+"
+   puts "addition of #{num1} and #{num2} is #{addition(num1,num2)} " 
+elsif operator=="-"
+    puts "subtraction of #{num1} and #{num2} is #{subtraction(num1,num2)} "
+elsif operator=="*"
+    puts "multiplication of #{num1} and #{num2} is #{multiplication(num1,num2)} "
+elsif operator=="\/"
+    puts "division of #{num1} and #{num2} is #{division(num1,num2)} "
+elsif operator=="%"
+    puts "modulus of #{num1} and #{num2} is #{modulus(num1,num2)} "
+else
+    puts "you have entered invalid operator"
+end
+
+
+
+
